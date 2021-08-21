ec9738a02ee7af34659838a95284d50939706d37
Whitman Lui <luiwhitman@gmail.com>
2021-08-21 14:20:44 +0800

    implement work experience

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -37,17 +37,150 @@ class WorkExperienceSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          item.companyName,
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
-        Divider(
-          color: Theme.of(context).primaryColor,
-        ),
-      ],
+    return IntrinsicHeight(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 15,
+                  height: 15,
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: VerticalDivider(
+                    width: 3,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      item.title,
+                      style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    Text(
+                      item.companyName,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  item.timePeriod ?? '',
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption!
+                      .copyWith(fontSize: 14.0),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Achievements/Job Duties:',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const SizedBox(height: 5),
+                if (item.achievements != null) ...[
+                  Text(
+                    item.achievements ?? '',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  const SizedBox(height: 20),
+                ],
+                Container(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Wrap(
+                    runSpacing: 8.0,
+                    children: item.jobDuties!
+                        .map(
+                          (e) => Row(
+                            children: [
+                              Container(
+                                width: 6,
+                                height: 6,
+                                margin: const EdgeInsets.only(right: 10),
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  e,
+                                  style: Theme.of(context).textTheme.bodyText1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Technologies used:',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const SizedBox(height: 5),
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  children: item.technologiesUsed!
+                      .map(
+                        (e) => Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 5,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50.0),
+                            ),
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          child: Text(
+                            e,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2!
+                                .copyWith(fontSize: 12.0),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
